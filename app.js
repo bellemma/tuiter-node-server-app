93227f330a21909432347c1f0e05c5f15337433a
bellemma <bell.e@northeastern.edu>
2022-11-30 23:05:04 -0500

    a9 cors

--- app.js
+++ app.js
@@ -7,9 +7,7 @@ import UserController
 import TuitsController
   from "./controllers/tuits/tuit-controller.js";
 import mongoose from "mongoose";
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
-    || 'mongodb://localhost:27017/tuiter'
-mongoose.connect(CONNECTION_STRING);
+
 
 const app = express()
 app.use(express.json()); // parse JSON from http req body
@@ -17,4 +15,8 @@ app.use(cors());
 HelloController(app);
 UserController(app);
 TuitsController(app);
+
 app.listen(process.env.PORT || 4000);
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+    || 'mongodb://localhost:27017/tuiter'
+mongoose.connect(CONNECTION_STRING);

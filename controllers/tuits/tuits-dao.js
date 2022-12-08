b2f4bdd4dd0fb4f0a725013d46b59a48541c00bd
bellemma <bell.e@northeastern.edu>
2022-12-08 12:25:53 -0500

    dao

--- controllers/tuits/tuits-dao.js
+++ controllers/tuits/tuits-dao.js
@@ -3,4 +3,5 @@ export const findTuits = () => tuitsModel.find();
 export const createTuit = (tuit) => tuitsModel.create(tuit);
 export const deleteTuit = (tid) => tuitsModel.deleteOne({_id: tid});
 export const updateTuit = (tid, tuit) =>
-    tuitsModel.updateOne({_id: tid}, {$set: tuit})
+    tuitsModel.updateOne({_id: tid}, {$set: tuit});
+export const hello = (tid, tuit) => tuitsModel.find( {salary:true, _id:false})
